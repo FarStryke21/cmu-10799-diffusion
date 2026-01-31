864ade90982cb19fb05951ce55143aec72e776e6
FarStryke21 <aman.chulawala@gmail.com>
2026-01-31 10:18:00 -0500

    PS2 - Flow Matching Submit Version

--- src/methods/flow_matching.py
+++ src/methods/flow_matching.py
@@ -37,18 +37,13 @@ class FlowMatching(BaseMethod):
         x_0 = torch.randn_like(x_1)
         
         # 3. Compute x_t (Linear Interpolation)
-        # x_t = (1 - t) * x_0 + t * x_1
-        # Need to reshape t for broadcasting: (B,) -> (B, 1, 1, 1)
         t_view = t.view(-1, *([1] * (x_1.ndim - 1)))
         x_t = (1 - t_view) * x_0 + t_view * x_1
         
         # 4. Compute Ground Truth Velocity
-        # v_t = d/dt (x_t) = x_1 - x_0
         target_velocity = x_1 - x_0
         
         # 5. Predict Velocity
-        # Note: We scale t by 999 to match the input range expected by the UNet's 
-        # embedding layer (which was designed for DDPM's 0-1000 range).
         v_pred = self.model(x_t, t * 999)
         
         # 6. Loss (MSE)
@@ -74,22 +69,17 @@ class FlowMatching(BaseMethod):
         x = torch.randn((batch_size, *image_shape), device=self.device)
         
         # 2. Define time steps (0 to 1)
-        # using linspace ensures we hit exactly 0.0 and 1.0
         times = torch.linspace(0, 1, num_steps + 1, device=self.device)
         dt = 1.0 / num_steps
         
         # 3. Euler Integration Loop
         for i in range(num_steps):
             t = times[i]
-            
-            # Create batch of current time t
+
             t_batch = torch.ones((batch_size,), device=self.device) * t
             
-            # Predict velocity
-            # Again, scale t by 999 for the model
             v_pred = self.model(x, t_batch * 999)
             
-            # Update x: x_{t+1} = x_t + v(x_t, t) * dt
             x = x + v_pred * dt
             
         return x
